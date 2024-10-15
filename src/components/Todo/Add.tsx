d23d6e1609cc30a5240713852c9eed56f185dbf4
Kayani57 <157056734+Kayani57@users.noreply.github.com>
2024-10-15 16:08:33 +0500

    Update Add.tsx

--- src/components/Todo/Add.tsx
+++ src/components/Todo/Add.tsx
@@ -9,13 +9,13 @@ export function Add({ handleAdd }: AddProps) {
   return (
     <Button
       variant="contained"
-      color="primary" // You can change this to "secondary" or other colors as needed
+      color="primary" 
       onClick={handleAdd}
       sx={{
-        margin: 2, // Apply margin around the button
-        width: '100%', // Full width by default
+        margin: 2,
+        width: '100%', 
         '@media (min-width:600px)': {
-          width: 'auto', // Auto width on larger screens
+          width: 'auto',
         },
         fontWeight: 'bold',
       }}
